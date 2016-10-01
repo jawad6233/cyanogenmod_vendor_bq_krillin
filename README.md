02faef9c7e9d2f827694865c6db4a3b5b401a846
Pablito2020 <socdelbarca72@gmail.com>
2016-10-01 19:19:18 +0200

    add blobs

--- README.md
+++
@@ -1,2 +0,0 @@
-# android_vendor_bq_krillin
-Vendor blobs for bq Aquaris E4.5, AKA krillin
---
+++ krillin-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/krillin/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/krillin/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/krillin/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/krillin/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/krillin/xbin,system/xbin)
+
